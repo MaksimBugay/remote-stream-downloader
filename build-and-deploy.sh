27a2b15649c13c98deb7e97d8218a05381e93b8e
maksim.bugai <maksim.bugai@multiloginapp.com>
2026-01-20 07:32:18 +0200

    Docker build enhancements

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -7,3 +7,6 @@ docker image rm yt-dlp-streamer:latest
 docker build -t yt-dlp-streamer:latest .
 docker compose up -d
 docker logs -f yt-dlp-streamer
+
+#docker tag yt-dlp-streamer:latest n7fr846yfa6ohlhe/mbugai:remote-strean-downloader-1
+#docker push n7fr846yfa6ohlhe/mbugai:remote-strean-downloader-1
